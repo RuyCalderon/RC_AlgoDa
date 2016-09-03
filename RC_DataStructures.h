3bf07467ebca87441c62404873987a7ca78ac233
Ruy Calderon <ruy.calderon@gmail.com>
2016-09-03 11:44:39 -0500

    Linked Lists, single and double, first

--- RC_DataStructures.h
+++ RC_DataStructures.h
@@ -5,11 +5,47 @@
 
 namespace RC_DataStructures	{
 	class RC_LinkedList_Single {
+	public:
+		RC_LinkedList_Single();
+		~RC_LinkedList_Single();
 
+		void AddNode(int Val);
+		void InsertNode(int Val);
+		int Pop();
+		void ToHead();
+
+		int* Next();
+		bool Find(int Val);
+	private:
+		void ToTail();
+
+		struct Node {
+			int val;
+			Node *Next;
+
+			Node::Node(int Val = 0) : val(Val), Next(0) {};
+			Node::~Node();
+		};
+		Node Head;
+		Node Current;
 	};
 
 	class RC_LinkedList_Double {
+	public:
+		RC_LinkedList_Double();
+		~RC_LinkedList_Double();
 
+	private:
+		struct Node {
+			int val;
+			Node *Next;
+			Node *Previous;
+
+			Node::Node(int Val = 0, Node *Previous = 0, Node *Next = 0) : val(Val), Previous(Previous), Next(Next) {};
+			Node::~Node();
+		};
+		Node Head;
+		Node Current;
 	};
 
 	class RC_Tree {
---
+++ RC_LinkedList_Double.cpp
@@ -0,0 +1,5 @@
+#include "RC_DataStructures.h"
+
+RC_DataStructures::RC_LinkedList_Double::RC_LinkedList_Double() {
+
+}
